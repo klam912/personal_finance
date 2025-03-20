f31753f4d02edbd3c2d65a5634e55d8fccc69284
Ken Lam <kenlam@Kens-MacBook-Pro.local>
2025-03-20 10:40:36 -0500

    added viewing methods to view each table and removed the if main part

--- query.py
+++ query.py
@@ -2,20 +2,19 @@ import sqlite3
 import set_up_db
 import add_trans
 
-def fetch_data(query: str):
+def fetch_data(query: str, limit: int):
     # Connect to the database
     conn = sqlite3.connect("finance_tracker.db")
     cursor = conn.cursor()
 
     # Execute query
-    cursor.execute(query)
+    cursor.execute(query, (limit,))
 
     # Fetch all results
     rows = cursor.fetchall()
 
     # Print table
     if rows:
-        print("Row:")
         for row in rows:
             print(row)
     else:
@@ -23,32 +22,38 @@ def fetch_data(query: str):
 
     conn.close()
 
-if __name__ == "__main__":
-    print("Transactions:")
+def view_transactions(limit: 10):
     trans_query = """
     SELECT *
     FROM transactions
-"""
-    fetch_data(trans_query)
+    LIMIT ?
+    """
+    print("Transactions:")
+    fetch_data(trans_query, limit)
 
-    print("Daily:")
+def view_daily(limit: 10):
     daily_query = """
     SELECT *
     FROM daily
-"""
-    fetch_data(daily_query)
+    LIMIT ?
+    """
+    print("Daily summary:")
+    fetch_data(daily_query, limit)
 
-    print("Weekly:")
+def view_weekly(limit: 10):
     weekly_query = """
     SELECT *
     FROM weekly
-"""
+    LIMIT ?
+    """
+    print("Weekly summary:")
+    fetch_data(weekly_query, limit)
 
-    fetch_data(weekly_query)
-
-    print("Monthly:")
+def view_monthly(limit: 10):
     monthly_query = """
     SELECT *
     FROM monthly
-"""
-    fetch_data(monthly_query)
+    LIMIT ?
+    """
+    print("Monthly summary:")
+    fetch_data(monthly_query, limit)
