5de3aa52db73871c3b78af84d82b79105275e8be
Ken Lam <kenlam@Kens-MacBook-Pro.local>
2025-03-23 13:43:55 -0500

    run different test based on positional parameters

--- automate.sh
+++ automate.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Move to the db directory
-cd "$(dirname "$0")/db"
-
 # Run the python script
-python3 main.py
+if [ "$1" == "test_db" ]; then
+    # Move to the db directory
+    cd "$(dirname "$0")/db"
+    python3 main.py
+elif [ "$1" == "test_backend" ]; then
+    # Move to the backend directory
+    cd "$(dirname "$0")/backend"
+    uvicorn main:app --reload
+else
+    echo "Invalid argument. Please use 'test_db' or 'test_backend'."
+    exit 1
+fi
+
+exit 0
