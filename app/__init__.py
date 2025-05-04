3b313e659df218c2ab63e297f6da66ccb6986ebc
Ken Lam <kenlam@Kens-MacBook-Pro.local>
2025-05-04 01:52:59 -0400

    added an _init_ file for initialization of the module app.

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.add_entry import add_entry
+from app.routes import init_routes
+from app.supabase_client import supabase
+
+def create_app():
+    app = Flask(__name__)
+
+    # Add routes
+    init_routes(app)
+    return app
