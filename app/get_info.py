1cb584d1240ba1e21c01e4d6601637f92039b5c4
Ken Lam <kenlam@Kens-MacBook-Pro.local>
2025-05-05 12:58:13 -0400

    added a get_info function to retrieve the user-id

---
+++ app/get_info.py
@@ -0,0 +1,9 @@
+import supabase
+from app.supabase_client import supabase
+from flask import current_app
+
+def get_user_id(username :str):
+    """Get the user_id from the User table given the username"""
+    res = supabase.table('User').select('id').eq('username', username).execute()
+
+    return res
